1284da4af230216bf5aeed9b335b9c8530255b6b
Justin Brubaker <justinbrubaker7@gmail.com>
2021-03-08 20:39:02 -0800

    more javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 var userFormEl = document.querySelector('#user-form');
 var languageButtonsEl = document.querySelector('#language-buttons');
 var serchTerm = document.querySelector('#searchTerm');
+
+//these are for the div to display the data
 var repoContainerEl = document.querySelector('#repos-container');
 var repoSearchTerm = document.querySelector('#repo-search-term');
 
@@ -10,7 +12,7 @@ var formSubmitHandler = function (event) {
     var search = serchTerm.value.trim();
   
     if (search) {
-      getUserRepos(search);
+      getInfo(search);
   
       repoContainerEl.textContent = '';
       serchTerm.value = '';
@@ -23,7 +25,7 @@ var formSubmitHandler = function (event) {
 var getInfo = function (query) {
 
     
-    var apiUrl = "https://www.loc.gov/newspapers/?q="+ query +"&fo=json"
+    var apiUrl = "https://www.loc.gov/search/?q="+ query +"&fo=json"
 
 
     fetch(apiUrl).then(function (response) {
@@ -39,40 +41,40 @@ var getInfo = function (query) {
 
 
 
-  var displayRepos = function (repos, searchTerm) {
-    if (repos.length === 0) {
+  var displayRepos = function (Searches1, searchTerm) {
+    if (Searches1.length === 0) {
       repoContainerEl.textContent = 'No searches found.';
 
       return;
     }
   
-    repoSearchTerm.textContent = searchTerm;
+    Searches1earchTerm.textContent = searchTerm;
   
-    for (var i = 0; i < repos.length; i++) {
+    for (var i = 0; i < Searches1.length; i++) {
 
-      var repoName = repos[i].owner.login + '/' + repos[i].name;
-      console.log(repoName)
+      var searchName = Searches1[i].link + '/' + Searches1[i].title;
+      console.log(searchName)
   
       var repoEl = document.createElement('div');
       repoEl.classList = 'list-item flex-row justify-space-between align-center';
   
       var titleEl = document.createElement('span');
-      titleEl.textContent = repoName;
+      titleEl.textContent = searchName;
   
       repoEl.appendChild(titleEl);
   
       var statusEl = document.createElement('span');
       statusEl.classList = 'flex-row align-center';
   
-      if (repos[i].open_issues_count > 0) {
-        statusEl.innerHTML =
-          "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
-      } else {
-        statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-      }
-  
       repoEl.appendChild(statusEl);
   
       repoContainerEl.appendChild(repoEl);
     }
   };
+
+
+
+
+
+
+  userFormEl.addEventListener('submit', formSubmitHandler);
